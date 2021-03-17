7d48eb8992e388a4a76d859370bfa7b10bd39588
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-03-16 23:47:33 -0700

    added stacked area chart component

--- recharts-playground/src/App.js
+++ recharts-playground/src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import LineChartComponent from './LineChartComponent'
 import BarChartComponent from './BarChartComponent'
+import StackedAreaChartComponent from './StackedAreaChartComponent'
 
 function App() {
   return (
     <>
       <LineChartComponent />
       <BarChartComponent />
+      <StackedAreaChartComponent />
     </>
   )
 }
